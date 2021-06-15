76ab0e8982b97b531cafedf9d779243b3ae3cc05
devan <devandewananta@gmail.com>
2021-06-15 08:06:50 +0700

    tambah menu pengajuan izin

---
+++ resources/views/home/pengajuanizin.blade.php
@@ -0,0 +1,16 @@
+
+@section('title', 'Dinas Kominfo Wonosobo | Hubungi Kami')
+@extends('layouts/main')
+@section('isi')
+@section('kondisi')
+<header id="header" class="header header-box-shadow-on-scroll header-abs-top header-bg-transparent header-black-nav-links-lg header-show-hide"
+          data-hs-header-options='{
+              "fixMoment": 1000,
+              "fixEffect": "slide"
+            }'>
+@endsection
+
+@section('kondisi2')
+<i class="fas fa-user-circle fa-lg" style="color:black"></i>
+@endsection
+
