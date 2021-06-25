f311c8a6234988852db995628d2fb07791e755d6
devan <devandewananta@gmail.com>
2021-06-25 09:02:59 +0700

    fungsi email

--- app/Http/Controllers/Tiket/TiketController.php
+++ app/Http/Controllers/Tiket/TiketController.php
@@ -164,14 +164,20 @@ class TiketController extends Controller
         $templateProcessor = new TemplateProcessor($path);
         $templateProcessor->setValues([
             'no' => $data->no,
+            'pelapor' => $data->pelapor,
+            'instansi' => $data->instansi,
+            'email' => $data->email,
+            'telepon' => $data->cp,
+            'petugas' => $data->petugas->name,
+            'tanggal' => \Carbon\Carbon::createFromTimeStamp(strtotime($data->created_at))->isoFormat('D MMMM Y'),
             'tahun' => date('Y', strtotime($data->created_at))
         ]);
 
         $templateProcessor->saveAs($pathSave);
-        $converter = new OfficeConverter($pathSave);
-        $converter->convertTo('aaaa.pdf'); //generates pdf file in same directory as test-file.docx
+        // $converter = new OfficeConverter($pathSave);
+        // $converter->convertTo('aaaa.pdf'); //generates pdf file in same directory as test-file.docx
 //        $converter = new OfficeConverter('test-file.docx', 'path-to-outdir');
-//        return response()->download($pathSave,$data->no.'.docx')->deleteFileAfterSend(true);
+       return response()->download($pathSave,$data->no.'.docx')->deleteFileAfterSend(true);
 
     }
 }
--- resources/views/home/pengajuanizin.blade.php
+++ resources/views/home/pengajuanizin.blade.php
@@ -18,63 +18,128 @@
   <main id="content" role="main" class="ignielPelangi">
     <!-- Contact Form Section -->
     <div class="container space-top-3 space-bottom-2">
-      <div class="row">
 
-            <div class="col-md-12 d-flex justify-content-center mb-5">
+      <!-- Nav -->
+<div class="d-flex justify-content-center text-center">
+  <ul class="nav nav-pills mb-7" role="tablist">
+    <li class="nav-item">
+      <a class="nav-link active" id="pills-one-code-features-example2-tab" data-toggle="pill" href="#pills-one-code-features-example2" role="tab" aria-controls="pills-one-code-features-example2" aria-selected="true">Daftar Pengajuan Izin</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" id="pills-two-code-features-example2-tab" data-toggle="pill" href="#pills-two-code-features-example2" role="tab" aria-controls="pills-two-code-features-example2" aria-selected="false">Lacak Nomor Tiket</a>
+    </li>
+  </ul>
+</div>
+<!-- End Nav -->
+
+<!-- Tab Content -->
+<div class="tab-content">
+  <div class="tab-pane fade show active" id="pills-one-code-features-example2" role="tabpanel" aria-labelledby="pills-one-code-features-example2-tab">
+   
+     <div class="col-md-12 d-flex justify-content-center mb-5">
       <h2>DAFTAR PENGAJUAN IZIN DISKOMINFO</h2>
       </div>
 
-      <div class="col-md-3">
+      <div class="row">
 
-      <a href="{{route('perijinan:akses.dc.create')}}">
+      <div class="col-md-3">
+            <a href="{{route('perijinan:akses.dc.create')}}">
             <img  class="card-img transition-zoom-hover" src="{{asset('front/assets/images/akses data center.jpg')}}" alt="Girl in a jacket" >
             </a>
-</div>
+      </div>
 
-<div class="col-md-3">
-
-      <a href="{{route('perijinan:permintaan.col.create')}}">
+      <div class="col-md-3">
+            <a href="{{route('perijinan:permintaan.col.create')}}">
             <img  class="card-img transition-zoom-hover" src="{{asset('front/assets/images/colocation.jpg')}}" alt="Girl in a jacket" >
             </a>
-</div>
+      </div>
 
-<div class="col-md-3">
-
-        <a href="{{route('perijinan:kunjungan.dc.create')}}">
+      <div class="col-md-3">
+            <a href="{{route('perijinan:kunjungan.dc.create')}}">
             <img  class="card-img transition-zoom-hover" src="{{asset('front/assets/images/kunjungan.jpg')}}" alt="Girl in a jacket" >
             </a>
-</div>
+      </div>
 
-<div class="col-md-3 mb-3">
-
-      <a href="{{route('perijinan:layanan.server.create')}}">
+      <div class="col-md-3 mb-3">
+            <a href="{{route('perijinan:layanan.server.create')}}">
             <img  class="card-img transition-zoom-hover" src="{{asset('front/assets/images/layanan.jpg')}}" alt="Girl in a jacket" >
             </a>
-</div>
+      </div>
 
-<div class="col-md-3">
-
-      <a href="{{route('perijinan:vps.baru.create')}}">
+      <div class="col-md-3">
+            <a href="{{route('perijinan:vps.baru.create')}}">
             <img  class="card-img transition-zoom-hover" src="{{asset('front/assets/images/vps baru.jpg')}}" alt="Girl in a jacket" >
             </a>
-</div>
+      </div>
 
-
-<div class="col-md-3">
-
-      <a href="{{route('perijinan:perubahan.vps.create')}}">
-            <img  class="card-img transition-zoom-hover" src="{{asset('front/assets/images/perubahan.jpg')}}" alt="Girl in a jacket" >
+      <div class="col-md-3">
+            <a href="{{route('perijinan:perubahan.vps.create')}}">
+            <img class="card-img transition-zoom-hover" src="{{asset('front/assets/images/perubahan.jpg')}}" alt="Girl in a jacket" >
             </a>
-</div>
+      </div>
 
-<div class="col-md-3">
-
-      <a href="{{route('perijinan:pengajuan.server.create')}}">
+      <div class="col-md-3">
+            <a href="{{route('perijinan:pengajuan.server.create')}}">
             <img  class="card-img transition-zoom-hover" src="{{asset('front/assets/images/pengantar.jpg')}}" alt="Girl in a jacket" >
             </a>
-</div>
+      </div>
 
+      </div>
+  </div>
+
+<!-- TAB 2 -->
+  <div class="tab-pane fade" id="pills-two-code-features-example2" role="tabpanel" aria-labelledby="pills-two-code-features-example2-tab">
+  <div class="row d-flex justify-content-center">
+  <div class="col-md-6 ">
+      <form class="input-group input-group-sm input-group-merge input-group-flush mb-3" action="/search" method="get">
+
+      <input name="q" type="search" class="form-control" placeholder="Masukkan Nomor Tiket" aria-label="Search articles" aria-describedby="searchLabel">
+      <div class="input-group-append">
+            <div class="input-group-text" id="searchLabel">
+            <button type="submit" class="no-border btn-transparent"><i class="fas fa-search"></i></button>
+            </div>
+      </div>
+      </form>
+
+      <!-- Step -->
+<ul class="step">
+  <li class="step-item">
+    <div class="step-content-wrapper">
+      <span class="step-icon step-icon-soft-primary">1</span>
+      <div class="step-content">
+        <h4>First step</h4>
+        <p class="step-text">Achieve virtually any design and layout from within the one template.</p>
+      </div>
+    </div>
+  </li>
+
+  <li class="step-item">
+    <div class="step-content-wrapper">
+      <span class="step-icon step-icon-soft-primary">2</span>
+      <div class="step-content">
+        <h4>Second step</h4>
+        <p class="step-text">We strive to figure out ways to help your business grow through all platforms.</p>
+      </div>
+    </div>
+  </li>
+
+  <li class="step-item">
+    <div class="step-content-wrapper">
+      <span class="step-icon step-icon-soft-primary">3</span>
+      <div class="step-content">
+        <h4>Third step</h4>
+        <p class="step-text">Find what you need in one template and combine features at will.</p>
+      </div>
+    </div>
+  </li>
+</ul>
+<!-- End Step -->
 </div>
+   </div>
+  </div>
+<!-- END TAB 2 -->
+</div>
+<!-- End Tab Content -->
     </div>
     <!-- End Contact Form Section -->
   </main>
