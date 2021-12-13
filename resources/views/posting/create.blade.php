fec0973d56167aca742a9c2626c69feeb51cbde7
Damar Wisnu Candra Prabowo <damar.wisnu1@gmail.com>
2021-12-13 15:50:07 +0700

    fix ck editor

--- resources/views/posting/create.blade.php
+++ resources/views/posting/create.blade.php
@@ -152,6 +152,7 @@
 @endsection
 
 @push('js')
+<script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="{{ asset('vendor/jsvalidation/js/jsvalidation.js')}}"></script>
