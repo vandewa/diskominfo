8abdaf9c2f9320c13c9a4ca29fc19c0d2f6df4fc
Damar Wisnu Candra Prabowo <damar.wisnu1@gmail.com>
2022-04-27 14:23:04 +0700

    iklan ramadan

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -73,10 +73,11 @@
 
   <!-- CSS Front Template -->
   <link rel="stylesheet" href="{{ url('front/assets/css/theme.min.css')}}">
-  <script src="https://www.google.com/recaptcha/api.js"></script>
-  {{-- <script src="{{ asset ('css/import/api.js')}}"></script> --}}
+  <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 
-  {!! ReCaptcha::htmlScriptTagJsApi() !!}
+  {{-- {!! ReCaptcha::htmlScriptTagJsApi() !!} --}}
+
+  {!! htmlScriptTagJsApi()  !!}
 
     <style>
       .buttonnya {
@@ -92,6 +93,18 @@
       }
       .help {cursor: help;}
     </style>
+
+    <noscript><style id="rocket-lazyload-nojs-css">.rll-youtube-player, [data-lazy-src]{display:none !important;}</style></noscript><script type="rocketlazyloadscript">
+/*! loadCSS rel=preload polyfill. [c]2017 Filament Group, Inc. MIT License */
+(function(w){"use strict";if(!w.loadCSS){w.loadCSS=function(){}}
+var rp=loadCSS.relpreload={};rp.support=(function(){var ret;try{ret=w.document.createElement("link").relList.supports("preload")}catch(e){ret=!1}
+return function(){return ret}})();rp.bindMediaToggle=function(link){var finalMedia=link.media||"all";function enableStylesheet(){link.media=finalMedia}
+if(link.addEventListener){link.addEventListener("load",enableStylesheet)}else if(link.attachEvent){link.attachEvent("onload",enableStylesheet)}
+setTimeout(function(){link.rel="stylesheet";link.media="only x"});setTimeout(enableStylesheet,3000)};rp.poly=function(){if(rp.support()){return}
+var links=w.document.getElementsByTagName("link");for(var i=0;i<links.length;i++){var link=links[i];if(link.rel==="preload"&&link.getAttribute("as")==="style"&&!link.getAttribute("data-loadcss")){link.setAttribute("data-loadcss",!0);rp.bindMediaToggle(link)}}};if(!rp.support()){rp.poly();var run=w.setInterval(rp.poly,500);if(w.addEventListener){w.addEventListener("load",function(){rp.poly();w.clearInterval(run)})}else if(w.attachEvent){w.attachEvent("onload",function(){rp.poly();w.clearInterval(run)})}}
+if(typeof exports!=="undefined"){exports.loadCSS=loadCSS}
+else{w.loadCSS=loadCSS}}(typeof global!=="undefined"?global:this))
+</script>
 </head>
 
 <body class="d-flex flex-column min-vh-100">
