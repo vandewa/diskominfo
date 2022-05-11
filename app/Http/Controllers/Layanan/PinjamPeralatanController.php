8c647d05589c41c0b00eee9e83857985067644b6
Damar Wisnu Candra Prabowo <damar.wisnu1@gmail.com>
2022-05-11 09:35:21 +0700

    typo di peraltan controller

--- app/Http/Controllers/Layanan/PinjamPeralatanController.php
+++ app/Http/Controllers/Layanan/PinjamPeralatanController.php
@@ -309,7 +309,7 @@ class PinjamPeralatanController extends Controller
             'alat' => strtoupper($data->alat),
             'tanggal' => strtoupper(\Carbon\Carbon::createFromTimeStamp(strtotime($data->created_at))->isoFormat('D MMMM Y')),
             'tanggal_mulai' => strtoupper(\Carbon\Carbon::createFromTimeStamp(strtotime($data->created_at))->isoFormat('D MMMM Y')),
-            'tanggal_selesai' => \strtoupper(\Carbon\Carbon::createFromTimeStamp(strtotime($data->created_at))->isoFormat('D MMMM Y')),
+            'tanggal_selesai' => strtoupper(\Carbon\Carbon::createFromTimeStamp(strtotime($data->created_at))->isoFormat('D MMMM Y')),
             'tahun' => date('Y', strtotime($data->created_at))
         ]);
 
